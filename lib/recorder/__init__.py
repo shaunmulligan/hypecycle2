e5f33ecef34572aa6bc03250949ef856be49c718
Shaun Mulligan <switchflip.shaun@gmail.com>
2022-10-16 16:26:09 +0100

    change location to use state.gps_altitude

--- lib/recorder/__init__.py
+++ lib/recorder/__init__.py
@@ -9,7 +9,7 @@ async def monitor_recording(state, interval=1):
         active_ride = await Rides.objects.filter(active=True).get_or_none()
         if active_ride:
             print("Ride started at {}, recording data to DB".format(active_ride.start_time))
-            location = Gpsreadings(ride_id=active_ride.id,latitude=state.latitude,longitude=state.longitude,speed=state.speed,altitude=state.altitude)
+            location = Gpsreadings(ride_id=active_ride.id,latitude=state.latitude,longitude=state.longitude,speed=state.speed,altitude=state.gps_altitude)
             hr_reading = Hrreadings(ride_id=active_ride.id, bpm=state.bpm)
             pow_reading = Powerreadings(ride_id=active_ride.id, power=state.instantaneous_power, cadence=state.cadence)
             env_reading = Enviroreadings(ride_id=active_ride.id, temp=state.temperature, altitude=state.altitude)
