158cc96391672d48d8141384a04b5dc0ccb721d3
Shaun Mulligan <switchflip.shaun@gmail.com>
2022-11-30 11:05:20 +0000

    add json endpoint for geojson

--- api/geojson.py
+++ api/geojson.py
@@ -1,4 +1,4 @@
-import io, shutil
+import io, shutil, json
 from fastapi import APIRouter, HTTPException
 from fastapi.responses import FileResponse
 import config
@@ -8,6 +8,17 @@ router = APIRouter()
 
 some_file_path = "data/current-ride.geojson"
 shutil.copy('data/default.geojson', 'data/current-ride.geojson') #ensure out current-ride.geojson is populated
+
+@router.get("/")
+async def get_current_ride_geojson():
+    cur_ride = await Rides.objects.filter(active=True).get_or_none()
+    
+    if not cur_ride:
+        return {"type": "FeatureCollection", "features": []}
+    else:
+        with open("data/current-ride.geojson", "r") as file:
+            data = json.load(file)
+            return data
 
 @router.get("/file", )
 async def get_current_ride_geojson_file():
