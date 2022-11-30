05a90bcc464f16772fd703dd8f82977d28e273c1
Shaun Mulligan <switchflip.shaun@gmail.com>
2022-11-30 11:43:56 +0000

    add comments on camera API

--- api/camera.py
+++ api/camera.py
@@ -17,12 +17,14 @@ loop = asyncio.get_running_loop()
 
 @router.get("/videos/", response_model=List[str])
 async def get_list_of_videos():
+    """Get a list of video files in data/videos"""
     videos = os.listdir('data/videos')
     videos.remove('info.txt')
     return videos
 
 @router.get("/videos/{filename}")
 async def download_a_video(filename: str):
+    """Download a video given its filename"""
     videos = os.listdir('data/videos')
     if (filename in videos):
         return FileResponse("data/videos/"+filename)
@@ -31,6 +33,7 @@ async def download_a_video(filename: str):
 
 @router.delete("/videos/")
 async def delete_all_videos():
+    """Delete all videos files"""
     files = glob.glob('data/videos/*.mp4', recursive=True)
     for f in files:
         try:
@@ -42,12 +45,14 @@ async def delete_all_videos():
 
 @router.get("/images/", response_model=List[str])
 async def get_list_of_images():
+    """Get a list of all the images in data/images"""
     images = os.listdir('data/images')
     images.remove('info.txt')
     return images
 
 @router.get("/images/{filename}")
 async def download_an_image(filename: str):
+    """Download an image given its filename"""
     images = os.listdir('data/images')
     if (filename in images):
         return FileResponse("data/images/"+filename)
@@ -56,6 +61,7 @@ async def download_an_image(filename: str):
 
 @router.delete("/images")
 async def delete_all_images():
+    """Delete all images in data/images"""
     files = glob.glob('data/images/*.jpg', recursive=True)
     for f in files:
         try:
@@ -67,6 +73,7 @@ async def delete_all_images():
 
 @router.post("/record")
 async def start_video_recording():
+    """Start a video recording and write it to a timestamped file"""
     global picam2
     encoder = H264Encoder(10000000)
     picam2.configure(video_config)
@@ -77,12 +84,14 @@ async def start_video_recording():
 
 @router.post("/stop")
 async def stop_video_recording():
+    """Stop Video recording and save file"""
     global picam2
     picam2.stop_recording()
     return "OK"
 
 @router.get("/snap")
 async def take_a_photo():
+    """Take a still photo with the camera"""
     global picam2
     picam2.start()
     filename = "data/images/" + datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p") + ".jpg"
