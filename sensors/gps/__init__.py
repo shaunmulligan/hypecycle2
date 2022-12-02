c1e842a73d1474413a96c027839f61da026a2660
Shaun Mulligan <switchflip.shaun@gmail.com>
2022-12-02 15:27:41 +0000

    GPS: catch TimeoutError and retry

--- sensors/gps/__init__.py
+++ sensors/gps/__init__.py
@@ -3,26 +3,31 @@ import board
 import busio
 import asyncio
 from pa1010d import PA1010D
-
+import logging
+logger = logging.getLogger(__name__)
 
 gps = PA1010D()
 
 async def monitor_gps(state):
     loop = asyncio.get_running_loop()
     while True:
-        result = await loop.run_in_executor(None, gps.update)
-        if result:
-            state.latitude = gps.latitude
-            state.longitude = gps.longitude
-            state.gps_altitude = gps.altitude
-            state.fix_quality = gps.gps_qual
-            state.location = {
-                "latitude": gps.latitude,
-                "longitude": gps.longitude,
-            }
-            state.latitude = gps.latitude
-            state.longitude = gps.longitude
-            state.speed = (gps.speed_over_ground*1.852000) if gps.speed_over_ground else 0.0 # Convert knots to km/h
+        try:
+            result = await loop.run_in_executor(None, gps.update)
+            if result:
+                state.latitude = gps.latitude
+                state.longitude = gps.longitude
+                state.gps_altitude = gps.altitude
+                state.fix_quality = gps.gps_qual
+                state.location = {
+                    "latitude": gps.latitude,
+                    "longitude": gps.longitude,
+                }
+                state.latitude = gps.latitude
+                state.longitude = gps.longitude
+                state.speed = (float(gps.speed_over_ground)*1.852000) if gps.speed_over_ground else 0.0 # Convert knots to km/h
+        except TimeoutError as e:
+            logger.warn("TimeoutError: ", e)
+            logger.warn("Trying again...")
         await asyncio.sleep(1.0)
 
 
