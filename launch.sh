88e410367fe5241e0107287feef9d5cbc1003da0
Shaun Mulligan <switchflip.shaun@gmail.com>
2023-01-06 07:45:31 +0000

    fix up i2c symlinking in launch.sh

--- launch.sh
+++ launch.sh
@@ -3,7 +3,7 @@ FILE=/dev/i2c-1
 FILE2=/dev/i2c-11
 if ! [ -L $FILE ]; then
     echo "No $FILE found, creating symlink"
-    if ! [-L $FILE2]; then
+    if ! [ -L $FILE2 ]; then
         echo "No i2c-11 found, trying i2c-22"
         sudo ln -s /dev/i2c-11 /dev/i2c-1
     else
@@ -16,4 +16,4 @@ fi
 # Clear BLE devices cache before start
 sudo rm -rf /var/lib/bluetooth/B8:27:EB:79:D0:BF/cache
 
-python -m uvicorn main:app --reload --workers 1 --host 0.0.0.0 --port 8001 --lifespan on
+python -m uvicorn main:app --workers 1 --host 0.0.0.0 --port 8001 --lifespan on  #--reload
